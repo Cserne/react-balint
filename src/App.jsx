0a09ed2f5391191933835bbf64f6b12d4a88de18
Cserne <angeli.cserne@gmail.com>
2022-01-10 10:39:01 +0100

    Changes added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css';
 import './index.css'
 
-function App() {
+let myName = "Bálint";
+let myNumber = 5;
+let myBoolean = true;
+let myArray = [1, 2, 3, "Béla", "Kázmér", true, false];
+
+const App = () => {
   return (
     <div>
       <h1>Hello world</h1>
+      <p>{ myName }</p>
+      <p>{ myNumber }</p>
+      <p>{ myBoolean }</p>
+      <p>{ myArray }</p>
       <p id="first">First paragraph</p>
       <p className="myp">Second paragraph</p>
       <p className="myp">Third paragraph</p>
