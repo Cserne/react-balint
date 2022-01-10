a2ee13a6c2e8bd8f1e10e56f51ec636880659901
Cserne <angeli.cserne@gmail.com>
2022-01-10 10:25:57 +0100

    Changes added.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p id="first">First paragraph</p>
+      <p className="myp">Second paragraph</p>
+      <p className="myp">Third paragraph</p>
+      <input type="password" placeholder="akarmi"></input>
+    </div>
+  );
+}
+
+export default App;
